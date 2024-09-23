4da86b410abc092ef96d52c8b5bb6e381e2e6b3c
Sebastian Mendoza <sebmendozaa@gmail.com>
2024-09-23 10:02:52 +0200

    scaffolding of cli parser and KvStore

---
+++ src/bin/kvs.rs
@@ -0,0 +1,43 @@
+use clap::{arg, command, Command};
+use std::process;
+
+fn main() {
+    let matches = command!()
+        .name(env!("CARGO_PKG_NAME"))
+        .about(env!("CARGO_PKG_DESCRIPTION"))
+        .author(env!("CARGO_PKG_AUTHORS"))
+        .version(env!("CARGO_PKG_VERSION"))
+        .subcommand(
+            Command::new("set")
+                .about("Give a key and value to store in database")
+                .arg(arg!([KEY] "The key as a string").required(true))
+                .arg(arg!([VALUE] "The value as string to store").required(true)),
+        )
+        .subcommand(
+            Command::new("get")
+                .about("Get a value given a ley as input")
+                .arg(arg!([KEY] "The key of the data point").required(true)),
+        )
+        .subcommand(
+            Command::new("rm")
+                .about("Remove a data point given a key as input")
+                .arg(arg!([KEY] "Key to identify and delete data point").required(true)),
+        )
+        .get_matches();
+
+    // println!("{:#?}", matches);
+
+    if let Some(matches) = matches.subcommand_matches("set") {
+        eprintln!("unimplemented");
+        process::exit(1);
+    } else if let Some(matches) = matches.subcommand_matches("get") {
+        eprintln!("unimplemented");
+        process::exit(1);
+    } else if let Some(matches) = matches.subcommand_matches("rm") {
+        eprintln!("unimplemented");
+        process::exit(1);
+    } else{
+        eprintln!("No recognizeable commands were run. Try cargo run -- --help for more info.");
+        process::exit(1)
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,38 @@
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
+
+
+
+pub struct KvStore {
+}
+
+impl KvStore {
+    pub fn new() -> KvStore {
+        KvStore {};
+        panic!()
+    }
+
+    pub fn set(&mut self, key: String, value: String) {
+        panic!()
+    }
+
+    pub fn get(&self, key: String) -> Option<String> {
+        panic!()
+    }
+
+    pub fn remove(&mut self, key: String) {
+        panic!()
+    }
+}
